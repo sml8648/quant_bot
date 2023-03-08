2dc9907ca9cc920171a7c27ccab8f6be1119398e
sangmun <sml8648@naver.com>
2023-03-08 17:00:19 +0900

    qq

--- crawl.py
+++ crawl.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 
     slack_notification('-----------------------------------')
 
-    print("test332")
+    print("3test332")
 
 # slack 알람까지 했단 말이지 그러면 뭘해야 되냐
 # airflow
