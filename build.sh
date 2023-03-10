cec905ca92b7ee14e28794d8f80bd11776d77398
sangmun <sml8648@naver.com>
2023-03-10 22:40:04 +0900

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 sudo docker build -t slack_alert .
-sudo docker docker run --name slack_alert -d -p 8000:8000 slack_alert:latest
+sudo docker run --name slack_alert -d -p 8000:8000 slack_alert:latest
